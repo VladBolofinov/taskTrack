9181a7473d5b68ad47ba2d61e67086313a0507b0
Vlad Bolofinov <vlad.bolofinov@gmail.com>
2023-01-04 16:07:02 +0300

    second commit

---
+++ src/components/app-input/app-input.jsx
@@ -0,0 +1,11 @@
+import './app-input.css';
+
+const AppInput = () => {
+    return (
+        <input className='app-input'
+               type="text"
+               placeholder='enter your new task'/>
+    )
+}
+
+export default AppInput;
---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import './app.css';
+import AppInput from '../app-input/app-input';
+function App() {
+    return (
+        <div className='app'>
+            <h1>Task Tracker made by Vlad Bolofinov</h1>
+            <AppInput/>
+        </div>
+    )
+}
+export default App;
